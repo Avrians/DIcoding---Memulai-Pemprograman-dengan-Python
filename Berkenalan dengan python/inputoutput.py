644d54038484d783f28a4dfc15834e1715fe1656
avrians <avriansyahb10@gmail.com>
2023-12-03 17:10:01 +0700

    [Av][first commit]

---
+++ Berkenalan dengan python/inputoutput.py
@@ -0,0 +1,16 @@
+name = input("Masukkan nama Anda: ")
+print("Halo", name)
+
+# Variabel ini menyimpan nama Rizky Khapidsyah
+nama = 'Rizky Khapidsyah'
+
+"""
+  Ini adalah komentar panjang
+  yang bisa digunakan untuk
+  memberikan penjelasan"""
+  
+# Ini adalah komentar satu baris
+
+'''
+  Ini adalah komentar panjang
+  dengan single quote'''
---
+++ Berkenalan dengan python/variabel.py
@@ -0,0 +1,6 @@
+greeting = "Goog Morning"
+print(greeting)
+
+addtion = 1 + 2
+result = 10 - 5
+print(result)
