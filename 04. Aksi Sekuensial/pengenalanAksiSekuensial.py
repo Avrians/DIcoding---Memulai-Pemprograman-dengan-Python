20b23313100d1aef41200180d828e9b72814fd5a
avrians <avriansyahb10@gmail.com>
2023-12-06 12:48:00 +0700

    [Av][04.01 & 04.02]

---
+++ 04. Aksi Sekuensial/pengenalanAksiSekuensial.py
@@ -0,0 +1,27 @@
+print("Selamat datang dalam program Python!\n")
+print("Silakan masukkan data diri Anda.")
+nama = input("Masukkan nama Anda: ")
+tahun_lahir = input("Masukkan tahun lahir Anda: ")
+umur = 2023 - int(tahun_lahir)
+ 
+print(f"Selamat datang {nama} dalam program Python, per 2023 umur Anda adalah {umur} tahun.\n")
+print("Terima kasih telah menggunakan program Python!")
+
+a = 6
+b = 9
+ 
+result = a + b
+print(result)
+
+b = 9
+a = 6
+
+result = a + b
+print(result)
+
+a = 6
+b = 9
+ 
+print(a**2)
+print(b//3)
+
---
+++ 04. Aksi Sekuensial/pythonInterpreter.py
@@ -0,0 +1,8 @@
+for i in range(10):
+    print(i)
+    
+teks = "Dicoding"
+Teks = "Indonesia"
+
+print(teks)
+print(Teks)
