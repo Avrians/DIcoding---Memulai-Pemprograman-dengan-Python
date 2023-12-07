ce6e0c2e83da1c7abc0c3bc66c1d1ad3745b46e6
avrians <avriansyahb10@gmail.com>
2023-12-07 14:12:19 +0700

    [av][05.1 Percabangan dan Ternary Operators]

---
+++ 05. Control Flow/percabanganDanTernaryOperators.py
@@ -0,0 +1,79 @@
+ketersediaan = "Daging ayam"
+
+if ketersediaan == "Daging ayam":
+    print("Ibu membeli dan memasak ayam")
+else:
+    print("Ibu membeli dan memasak tempe")
+    
+score = 100
+
+if score == 100:
+    print("Nilai Anda sempurna!")
+    
+x = ""
+
+if x:
+    print("Ini True")
+    
+score = 100
+
+if score == 100: print("Nilai Anda sempurna!")
+
+tinggi_badan = 120
+
+if tinggi_badan >=160:
+    print("Anda boleh menaiki roller coaster")
+else:
+    print("Anda tidak diperbolehkan menaiki roller coaster")
+    
+nilai = 65
+
+if nilai>=80:
+   print("Selamat! Anda mendapat nilai A")
+   print("Pertahankan!")
+elif nilai>=70:
+   print("Hore! Anda mendapat nilai B")
+   print("Tingkatkan!")
+elif nilai>=60:
+   print("Hmm.. Anda mendapat nilai C")
+   print("Ayo semangat!")
+else:
+   print("Waduh, Anda mendapat nilai D")
+   print("Yuk belajar lebih giat lagi!")
+   
+nilai = 81
+perilaku = 'tidak baik'
+
+if nilai>=80 and perilaku == 'baik':
+   print("Selamat! Anda mendapat nilai A dan telah berkelakuan baik")
+   print("Pertahankan!")
+elif nilai>=80 and perilaku != 'baik':
+   print("Kamu mendapatkan nilai A, tetapi perilaku Anda kurang baik")
+   print("Perbaiki lagi ya!")
+else:
+   print("Yuk belajar lebih giat lagi!")
+   
+# 
+# Ternary Operator
+# 
+lulus = True
+print("selamat") if lulus else print("perbaiki")
+
+# blok kode
+lulus = True
+if lulus:
+    print("Selamat") 
+else:
+    print("Perbaiki")
+    
+# Ternary TUples
+lulus = True
+kelulusan = ("Perbaiki, Anda belum lulus.","Selamat, Anda lulus!")[lulus]
+print(kelulusan)
+
+# versi blok kode
+lulus = True
+if lulus:
+    print("Selamat, Anda lulus!") 
+else:
+    print("Perbaiki, Anda belum lulus.")
