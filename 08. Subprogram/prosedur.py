3dccc9c6adf51a632407e14204157daec73c00c1
avrians <avriansyahb10@gmail.com>
2023-12-15 07:00:21 +0700

    [av][prosedur]

---
+++ 08. Subprogram/prosedur.py
@@ -0,0 +1,16 @@
+def greeting(name):
+    print("Halo " + name + ", Selamat Datang!")
+    
+def greeting2(name):
+    print("Halo " + name + ", Selamat Datang!")
+    return
+  
+def greeting(name):
+    print("Halo " + name + ", Selamat Datang!")
+
+greeting("Dicoding Indonesia")
+
+def greeting():
+    print("Halo Selamat Datang!")
+
+greeting()
